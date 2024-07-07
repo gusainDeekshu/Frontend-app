7100afa2ea40a5271958c4507ea7b06579431cc2
gusainDeekshu <gusaindeekshant@gmail.com>
2024-07-07 18:24:35 +0530

    second commit

--- front-end/src/components/Exploremenu/Exploremenu.jsx
+++ front-end/src/components/Exploremenu/Exploremenu.jsx
@@ -1,10 +1,24 @@
+import { menu_list } from '../../assets/assets'
 import  './Exploremenu.css'
 
 function Exploremenu() {
   return (
-    <div>
-      
-    </div>
+<div id="exploremenu" className="exploremenu">
+<h1>Explore our menu</h1>
+<p className='explore-menu-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia dolores corrupti atque sunt adipisci. Reprehenderit recusandae possimus veritatis assumenda minus, ratione neque aut quidem doloribus, similique a, esse eum ipsam!</p>
+<div className="explore-menu-list">
+  {menu_list.map((item,index)=>{
+    return (
+      <div key={index} className="explore-menu-list-item">
+        <img src={item.menu_image} alt="" />
+        <p>{item.menu_name}</p>
+
+      </div>
+    )
+  })}
+  </div>
+
+</div>
   )
 }
 
--- front-end/src/pages/home/Home.jsx
+++ front-end/src/pages/home/Home.jsx
@@ -2,11 +2,13 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
+import Exploremenu from '../../components/Exploremenu/Exploremenu'
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <Exploremenu/>
     </div>
   )
 }
